66d67ec70740b59e7426649bcb266c8978adf24e
Shaumik <sdaityari@gmail.com>
2014-06-10 20:15:56 +0530

    Cleaned JS code

--- atutor_api.js
+++ atutor_api.js
@@ -1,21 +1,38 @@
 var ATutorAPI = ATutorAPI || {};
 
 (function (api, $) {
-    $("#login-button").click(function (event) {
+
+    var constants = {
+        "urls" : {
+            "base" : "http://localhost/ATutor/",
+            "login" : "api/login/"
+        }, "selectors" : {
+            "login" : $("#login"),
+            "password" : $("#password"),
+            "loginButton" : $("#login-button")
+        }
+    }, accessToken;
+
+    constants.selectors.loginButton.click(function (event) {
+        // Prevent form submit
         event.preventDefault();
+
         $.ajax({
             type: "POST",
-            url: "http://localhost/ATutor/api/login/",
+            url: constants.urls.base + constants.urls.login,
             data: {
-                "username": $("#login").val(),
-                "password": $("#password").val()
+                "username": constants.selectors.login.val(),
+                "password": constants.selectors.password.val()
             },
             success: function (response) {
                 response = JSON.parse(response);
                 if (response.errorMessage) {
+                    // Assuming some error occurred
                     alert("Error: " + response.errorMessage);
                 } else if (response.access_token) {
-                    alert("Logged in. Access Token: " + response.access_token);
+                    // On successful login
+                    accessToken = response.access_token;
+                    alert("Logged in successfully.");
                 }
             }
         });
